eb939f775af2cf8306e56ac63a5391719539c2b3
Songyue Wang <me@songyue.wang>
2023-04-24 22:47:54 +0800

    Added README.md

--- ECDSA_Verify.sol
+++ ECDSA_Verify.sol
@@ -2,12 +2,11 @@
 pragma solidity ^0.8.3;
 
 contract VerifySignature {
-  function getMessageHash(string memory _message) public pure returns (bytes32) {
+  function getMessageHash(string memory _message) internal pure returns (bytes32) {
     return keccak256(abi.encodePacked(_message));
   }
 
-  
-  function getEthSignedMessageHash(bytes32 _messageHash) public pure returns (bytes32) {
+  function getEthSignedMessageHash(bytes32 _messageHash) internal pure returns (bytes32) {
     return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash));
   }
 
@@ -17,13 +16,37 @@ contract VerifySignature {
     return recoverSigner(ethSignedMessageHash, signature) == _signer;
   }
 
-  function recoverSigner(bytes32 _ethSignedMessageHash, bytes memory _signature) public pure returns (address) {
+  function getSlice(uint256 begin, uint256 end, string memory text) internal pure returns (string memory) {
+    bytes memory a = new bytes(end-begin+1);
+    for(uint i=0;i<=end-begin;i++){
+        a[i] = bytes(text)[i+begin-1];
+    }
+    return string(a);    
+  }
+
+  // given command string, parse into parameters to call
+  /*
+  function operationParser(string memory _message, string memory _sig) public returns (bool) {
+    Step 1. Check whether message string is in the correct format
+    Step 2. Check for correct signature recover(_message, _sig) == msg.sender
+    Step 3. Parse message into parameters
+    Step 4. Select the correct external contract and functino to call on behalf of the user
+  }
+  */
+
+  /*
+  function executeOperationExample(int param1, address param2, int param3) internal returns (bool) {
+    // call function in other contract
+  }
+  */
+
+  function recoverSigner(bytes32 _ethSignedMessageHash, bytes memory _signature) internal pure returns (address) {
     (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
 
     return ecrecover(_ethSignedMessageHash, v, r, s);
   }
 
-  function splitSignature(bytes memory sig) public pure returns (bytes32 r, bytes32 s, uint8 v) {
+  function splitSignature(bytes memory sig) internal pure returns (bytes32 r, bytes32 s, uint8 v) {
     require(sig.length == 65, "invalid signature length");
 
     assembly {
---
+++ README.md
@@ -0,0 +1,3 @@
+Off-chain (signing) demo: [please click here](https://sign.uniasset.io)
+
+On-chain (verification and execution) demo: please use [remix](https://remix.ethereum.org/) and copy `ECDSA_Verify.sol` there
